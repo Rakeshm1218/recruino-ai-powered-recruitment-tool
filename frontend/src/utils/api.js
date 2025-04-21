7deb56276dcf8c56d58d1af5e43a859bf720be7f
rakeshm888p@gmail.com <rakeshm888p@gmail.com>
2025-04-21 21:39:44 +0530

    frontend  deployment

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
+
+
 const api = axios.create({
-  baseURL: "http://localhost:5000/",
+  baseURL: "https://recruino-ai-powered-recruitment-tool-backend.vercel.app/",
 });
 
 // Add token to every request if available
